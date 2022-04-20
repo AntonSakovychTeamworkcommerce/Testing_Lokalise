2b7e15c0efa65807ad36854e3ea154edfe4d7b28
User <>
2022-04-20 15:22:27 +0300

    New Update

--- Testing_Lokalise/Testing_Lokalise/ViewController.swift
+++ Testing_Lokalise/Testing_Lokalise/ViewController.swift
@@ -11,6 +11,12 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
+
+        let test =  NSLocalizedString("welcome_screen_title", comment: "comment")
+
+        let test1 =  NSLocalizedString("login_button", comment: "comment")
+
         NotificationCenter.default.addObserver(
             self,
             selector: #selector(updateUserInterface),
